01656c90e4cf30377c6b89044356d0a21bf71280
Roman Tsegelskyi <roman.tsegelskyi@gmail.com>
2015-08-13 15:14:41 -0700

    Update brewer

--- .brewer.R
+++ .brewer.R
@@ -20,7 +20,7 @@ file.remove(list.files('plots/', full.names = TRUE))
 examples <- gsub("(.*)\\.brew$", "\\1", list.files("../pander/inst/examples/"))
 
 for (example in examples) {
-    brew_path <- file.path("../pander/inst/examples/", paste(example, ".brew", sep=""))
+    brew_path <- file.path("inst/examples/", paste(example, ".brew", sep=""))
     Pandoc.brew(brew_path, output = example, convert = 'html', open = FALSE)
     for (format in c('pdf', 'docx', 'odt')) {
         tryCatch(Pandoc.convert(example, format = format, open = FALSE), error = function(x) print(x))
